613dfa5e8ff5ed4dc00727d4e6c3ee630b6516d8
amirayat <amirayat20@gmail.com>
2023-04-10 19:28:13 +0330

    add ticket serializers

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,77 +1,126 @@
-from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from rest_framework.serializers import ModelSerializer, CharField, Serializer, PrimaryKeyRelatedField, ChoiceField, DictField, ListField
-from djoser.serializers import UserSerializer
-from profanity_filter import ProfanityFilter
-from chat.models import ChatUpload, Message, PredefindMessage
+from users.serializers import MemberSerializer
+from chat.models import ChatRoom
 from core.utils import fields
 
 
-UserModel = get_user_model()
+class ChatRoomSerializer(serializers.ModelSerializer):
+    """
+    base serializer for chat rooms  
+    """
+    members = MemberSerializer(many=True, read_only=True, source='some_members')
+    class Meta:
+        model = ChatRoom
+        fields = fields(model)+['members']
 
 
-class MessageContentSerializer(Serializer):
+class TicketSerializer(ChatRoomSerializer):
     """
-    serializer for message content
+    serializer for tickets
+    recieve [name,priority] from creator
     """
-    MESSAGE_CHOICES = (
-        ("user_typing", "user_typing"),
-        ("user_online", "user_online"),
-        ("user_notice", "user_notice"),
-        ("chat_message", "chat_message"),
+    TICKET_PRIORITY = (
+        ("LOW", "LOW"),
+        ("MEDIUM", "MEDIUM"),
+        ("HIGH", "HIGH"),
     )
-    type = ChoiceField(choices=MESSAGE_CHOICES)
-    detail = ListField(child=DictField(), required=False)
-    text = CharField(max_length=1000, allow_null=True,
-                     allow_blank=False, required=False)
-    file = PrimaryKeyRelatedField(
-        source='ChatUpload', allow_null=True, required=False, queryset=ChatUpload.objects.all())
-    reply_to = PrimaryKeyRelatedField(
-        source='Message', allow_null=True, required=False, queryset=Message.objects.all())
-    
-    def validate(self, attrs):
-        pf = ProfanityFilter()
-        if attrs["type"] == "chat_message" and attrs.get("text", None) is None and attrs.get("file", None) is None:
-            raise ValidationError(
-                detail="Message with no content is not valid.", code="user_notice")
-        elif attrs["type"] in ["user_typing", "user_online", "user_notice"] and attrs.get("detail", None) is None:
-            raise ValidationError(
-                detail="Message with no content is not valid.", code="user_notice")
-        if attrs.get("text", None) and pf.is_clean(attrs["text"]):
-            raise ValidationError(
-                detail="Message contain swear word.", code="user_notice")
-        return super().validate(attrs)
-
-
-class UploadSerializer(ModelSerializer):
-    """
-    serializer class to upload
-    """
-    class Meta:
-        model = ChatUpload
-        fields = fields(model)+['size']
-        read_only_fields = ("id",)
-
-
-class MessageSerializer(ModelSerializer):
-    """
-    serializer class for messages
-    """
-    sender = UserSerializer(read_only=True)
-    file = UploadSerializer(read_only=True, allow_null=True)
+    priority = serializers.ChoiceField(default="LOW",choices=TICKET_PRIORITY)
 
     class Meta:
-        model = Message
-        fields = fields(model)+['created_at']
-        read_only_fields = ("id",)
+        model = ChatRoom
+        fields = (
+            "id",
+            "name",
+            "closed",
+            "closed_at",
+            "priority",
+            "read_only",
+            "members",
+        )
+        read_only_fields = (
+            "id",
+            "closed",
+            "closed_at",
+            "read_only"
+        )
 
 
-class PredefindMessageSerializer(ModelSerializer):
+class CloseTicketSerializer(ChatRoomSerializer):
     """
-    serializer class for predefind messages
+    serializer for close ticket
     """
 
     class Meta:
-        model = PredefindMessage
-        fields = fields(model)
-        read_only_fields = ("id",)
+        model = ChatRoom
+        fields = (
+            "id",
+            "name",
+            "closed",
+            "closed_at",
+            "priority",
+            "read_only",
+            "members",
+        )
+        read_only_fields = (
+            "id",
+            "name",
+            "closed",
+            "closed_at",
+            "priority",
+            "read_only",
+            "members",
+        )
+
+class AssignStaffToTicketSerializer(serializers.ModelSerializer):
+    """
+    serializer for staff to assign new staff
+    """
+    members = MemberSerializer(many=True, source='some_members')
+
+    def is_valid(self, *, raise_exception=False):
+        assert hasattr(self, 'initial_data'), (
+            'Cannot call `.is_valid()` as no `data=` keyword argument was '
+            'passed when instantiating the serializer instance.'
+        )
+
+        if not hasattr(self, '_validated_data'):
+            try:
+                self._validated_data = self.run_validation(self.initial_data)
+            except ValidationError as exc:
+                self._validated_data = {}
+                self._errors = exc.detail
+            else:
+                self._errors = {}
+
+        if self._errors and raise_exception:
+            raise ValidationError(self.errors)
+
+        members = self.initial_data['members']
+        if len(members) != 1:
+            raise ValidationError({"members":"Insert single staff."})
+        member = members[0]
+        if not isinstance(member, dict) or not "username" in member.keys():
+            raise ValidationError({"members":"should contain item with valid 'username' field."})
+
+        return not bool(self._errors)
+
+    class Meta:
+        model = ChatRoom
+        fields = (
+            "id",
+            "name",
+            "closed",
+            "closed_at",
+            "priority",
+            "read_only",
+            "members",
+        )
+        read_only_fields = (
+            "id",
+            "name",
+            "closed",
+            "closed_at",
+            "priority",
+            "read_only",
+        )
