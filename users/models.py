678179611655666e044057e133135b822029769e
amirayat <amirayat20@gmail.com>
2023-04-11 13:13:16 +0330

    add user role property

--- users/models.py
+++ users/models.py
@@ -6,7 +6,24 @@ class ChatUser(AbstractUser):
     """
     model class for users
     """
+
+    def __init__(self, *args, **kwargs) -> None:
+        self._role = None
+        super().__init__(*args, **kwargs)
+
     photo = models.ImageField(upload_to='files', null=True)
+
+    @property
+    def role(self):
+        """
+        user role in a chat room
+        fill based on query
+        """
+        return self._role
+
+    @role.setter
+    def role(self, role=None):
+         self._role = role
     
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
