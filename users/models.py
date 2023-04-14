aaa41e70b2b7dbd3a9c31a5c9dfb3e3d8683639d
amirayat <amirayat20@gmail.com>
2023-04-14 23:30:36 +0330

    add action permission property to user model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from chat.permissions import permission, no_permission
 
 
 class ChatUser(AbstractUser):
@@ -9,6 +10,7 @@ class ChatUser(AbstractUser):
 
     def __init__(self, *args, **kwargs) -> None:
         self._role = None
+        self._action_permission = no_permission()
         super().__init__(*args, **kwargs)
 
     photo = models.ImageField(upload_to='files', null=True)
@@ -17,13 +19,34 @@ class ChatUser(AbstractUser):
     def role(self):
         """
         user role in a chat room
-        fill based on query
         """
         return self._role
 
     @role.setter
     def role(self, role=None):
          self._role = role
+
+    @property
+    def action_permission(self):
+        """
+        user action_permission in a chat room
+        """
+        return self._action_permission
+
+    @action_permission.setter
+    def action_permission(self, action_permission=None):
+         self._action_permission = action_permission
+
+    def has_action_permission(self, action: str) -> bool:
+        if self.action_permission == permission(action) == no_permission():
+            """
+            case of:
+                user action_permission property has not been set 
+                and
+                given action is not defined
+            """
+            return False
+        return True if self.action_permission % permission(action) == 0 else False
     
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
