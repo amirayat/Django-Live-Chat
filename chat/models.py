097f6a8383facccccfcaf3faf66e7959065b0a49
amirayat <amirayat20@gmail.com>
2023-04-14 23:27:33 +0330

    chat member action permissions

--- chat/models.py
+++ chat/models.py
@@ -2,16 +2,22 @@ from functools import lru_cache
 from typing import TypeVar
 from django.utils import timezone
 from django.db import models
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 from core.base_model import RootModel, RootModelManager
+from chat.permissions import (permission,
+                              creator_permissions,
+                              admin_permissions,
+                              member_permissions,
+                              no_permission)
 
 
 UserModel = get_user_model()
 
 
 ChatRoomObject = TypeVar("ChatRoomObject", bound=models.Model)
+ChatMemberObject = TypeVar("ChatMemberObject", bound=models.Model)
 
 
 class ChatRoomManager(RootModelManager):
@@ -30,23 +36,23 @@ class ChatRoomManager(RootModelManager):
 
     def create(self, creator: UserModel, **kwargs):
 
-        name = kwargs.get('name', str())
-        priority = kwargs.get('priority', 'LOW')
-        type = kwargs.get('type', 'USER_TICKET')
-        contact = kwargs.get('contact', creator)
-        members = kwargs.get('members', list())
+        _name = kwargs.get('name', str())
+        _priority = kwargs.get('priority', 'LOW')
+        _type = kwargs.get('type', 'USER_TICKET')
+        _contact = kwargs.get('contact', creator)
+        _members = kwargs.get('members', list())
 
         if type == 'USER_TICKET':
-            return self.create_ticket(name=name, priority=priority, creator=creator)
+            return self.create_ticket(name=_name, priority=_priority, creator=creator)
 
         elif type == 'PRIVATE_CHAT':
-            return self.create_private_chat(creator=creator, contact=contact)
+            return self.create_private_chat(creator=creator, contact=_contact)
 
         elif type == 'PUBLIC_GROUPE':
-            return self.create_group(name=name, creator=creator, members=members, type=type)
+            return self.create_group(name=_name, creator=creator, members=_members, type=type)
 
         elif type == 'PRIVATE_GROUPE':
-            return self.create_group(name=name, creator=creator, members=members, type=type)
+            return self.create_group(name=_name, creator=creator, members=_members, type=type)
 
     def create_private_chat(self, creator: UserModel, contact: UserModel) -> ChatRoomObject:
         """
@@ -54,36 +60,36 @@ class ChatRoomManager(RootModelManager):
         creator: request.user
         contact: another user 
         """
-        name = str(creator.id)+'.'+str(contact.id)+'.'+contact.username
-        private_chat, is_create = ChatRoom.objects.get_or_create(
-            name=name,
+        _name = str(creator.id)+'.'+str(contact.id)+'.'+contact.username
+        _private_chat, _is_create = ChatRoom.objects.get_or_create(
+            name=_name,
             type="PRIVATE_CHAT"
         )
-        if is_create:
-            ChatRoomMember.objects.bulk_create([
-                ChatRoomMember(is_creator=True, chat_room=private_chat,
-                            user=creator),
-                ChatRoomMember(is_creator=False, chat_room=private_chat,
-                            user=contact),
+        if _is_create:
+            ChatMember.objects.bulk_create([
+                ChatMember(is_creator=True, chat_room=_private_chat,
+                           user=creator),
+                ChatMember(is_creator=False, chat_room=_private_chat,
+                           user=contact),
             ])
-        return private_chat
+        return _private_chat
 
     def create_ticket(self, name: str, priority: str, creator: UserModel) -> ChatRoomObject:
         """
         create ticket for request user
         creator: request.user
         """
-        ticket = super().create(name=name, priority=priority, type="USER_TICKET")
+        _ticket = super().create(name=name, priority=priority, type="USER_TICKET")
         # find staff with less open ticket to assing to the current ticket
-        staff = UserModel.objects.filter(~Q(id=creator.id) & Q(is_staff=True))\
+        _staff = UserModel.objects.filter(~Q(id=creator.id) & Q(is_staff=True))\
             .annotate(c=Count("user_member")).order_by("c").first()
-        ChatRoomMember.objects.bulk_create([
-            ChatRoomMember(is_creator=True, chat_room=ticket,
-                           user=creator),
-            ChatRoomMember(is_creator=False, chat_room=ticket,
-                           user=staff),
+        ChatMember.objects.bulk_create([
+            ChatMember(is_creator=True, chat_room=_ticket,
+                       user=creator),
+            ChatMember(is_creator=False, chat_room=_ticket,
+                       user=_staff),
         ])
-        return ticket
+        return _ticket
 
     def create_group(self, name: str, creator: UserModel, members: list, type: str) -> ChatRoomObject:
         """
@@ -92,19 +98,23 @@ class ChatRoomManager(RootModelManager):
         members: [<ChatUser: user1>, <ChatUser: user2>, ...]
         type: PUBLIC_GROUPE or PRIVATE_GROUPE
         """
-        group = super().create(name=name, type=type)
+        _group = super().create(name=name, type=type)
         # create members
-        all_members = [ChatRoomMember(is_creator=True, is_admin=False, chat_room=group, user=creator)] + \
-            [ChatRoomMember(is_creator=False, chat_room=group, user=member)
+        _all_members = [ChatMember(is_creator=True, is_admin=False, chat_room=_group, user=creator, action_permission=creator_permissions())] + \
+            [ChatMember(is_creator=False, chat_room=_group, user=member)
              for member in members if member.id != creator.id]
-        ChatRoomMember.objects.bulk_create(all_members)
-        return group
+        ChatMember.objects.bulk_create(_all_members)
+        return _group
 
 
 class ChatRoom(RootModel):
     """
     class model for chat room
     """
+
+    def __init__(self, *args, **kwargs) -> None:
+        self._member = None
+        super().__init__(*args, **kwargs)
 
     TICKET_PRIORITY = (
         ("LOW", "LOW"),
@@ -182,11 +192,12 @@ class ChatRoom(RootModel):
         """
         return list of all chat room users
         """
-        qs = self.chat_room_member.select_related("user")
-        for obj in qs:
+        _query_set = self.chat_room_member.select_related("user")
+        for obj in _query_set:
             obj.user.role = obj.role
-        all_users = [obj.user for obj in qs]
-        return all_users
+            obj.user.action_permission = obj.action_permission
+        _all_users = [obj.user for obj in _query_set]
+        return _all_users
 
     @property
     def some_members(self) -> list:
@@ -194,28 +205,102 @@ class ChatRoom(RootModel):
         return some chat room users
         """
         if self.is_private_chat or self.is_ticket:
-            some_users = self.all_members
+            _some_users = self.all_members
         elif self.is_group:
-            some_users = self.all_members[:10]
-        return some_users
+            _some_users = self.all_members[:10]
+        return _some_users
+
+    @property
+    def member(self) -> ChatMemberObject:
+        """
+        return selected member
+        """
+        return self._member
+
+    def select_member(self, user_id: int) -> bool:
+        """
+        select a member
+        """
+        try:
+            self._member = self.chat_room_member.get(
+                chat_room_id=self.id,
+                user_id=user_id)
+            return True
+        except:
+            return False
 
     @property
     def admins(self) -> list:
         """
         return list of chat room admins
         """
-        admins = list(filter(lambda user: user.role ==
-                      "admin", self.all_members))
-        return admins
+        _admins = list(filter(lambda user: user.role ==
+                              "admin", self.all_members))
+        return _admins
+
+    @property
+    def admins_can_update(self) -> list:
+        """
+        return list of group admins who can update group
+        """
+        _admins = list(filter(
+            lambda user: user.role == "admin" and
+            user.has_action_permission("update_group"),
+            self.all_members))
+        return _admins
+
+    @property
+    def admins_can_close(self) -> list:
+        """
+        return list of group admins who can close group
+        """
+        _admins = list(filter(
+            lambda user: user.role == "admin" and
+            user.has_action_permission("close_group"),
+            self.all_members))
+        return _admins
+
+    @property
+    def admins_can_lock(self) -> list:
+        """
+        return list of group admins who can lock group
+        """
+        _admins = list(filter(
+            lambda user: user.role == "admin" and
+            user.has_action_permission("lock_group"),
+            self.all_members))
+        return _admins
+
+    @property
+    def admins_can_add(self) -> list:
+        """
+        return list of group admins who can add member
+        """
+        _admins = list(filter(
+            lambda user: user.role == "admin" and
+            user.has_action_permission("add_member"),
+            self.all_members))
+        return _admins
+
+    @property
+    def admins_can_remove(self) -> list:
+        """
+        return list of group admins who can remove member
+        """
+        _admins = list(filter(
+            lambda user: user.role == "admin" and
+            user.has_action_permission("remove_member"),
+            self.all_members))
+        return _admins
 
     @property
     def creator(self) -> UserModel:
         """
         return creator of chat room
         """
-        creator = list(filter(lambda user: user.role ==
-                       "creator", self.all_members))[0]
-        return creator
+        _creator = list(filter(lambda user: user.role ==
+                               "creator", self.all_members))[0]
+        return _creator
 
     @property
     def count_members(self) -> int:
@@ -236,7 +321,7 @@ class ChatRoom(RootModel):
         """
         return user in self.admins
 
-    def change_read_only(self) -> None:
+    def lock(self) -> None:
         """
         change chat room read only status
         """
@@ -248,10 +333,26 @@ class ChatRoom(RootModel):
 
     def close(self) -> None:
         """
-        close chat room and make it read only
+        close chat room
+        """
+        self.closed = True
+        self.save()
+
+    def close_lock(self) -> None:
+        """
+        close and lock chat room
         """
         self.closed = True
         self.read_only = True
+        self.save()
+
+    def close_lock_delete(self) -> None:
+        """
+        close and delete chat room and make it read only
+        """
+        self.closed = True
+        self.read_only = True
+        self.is_deleted = True
         self.save()
 
     def open(self) -> None:
@@ -265,32 +366,42 @@ class ChatRoom(RootModel):
 
     def join_public_group(self, user: UserModel) -> bool:
         """
-        user joins to a public group
-        update ChatRoomMember if user has left the group else create new
+        user joins to a public group if it is not removed
+        update ChatMember if user has left the group else create new
         """
         if self.is_public_group and not self.has_member(user):
-            self.chat_room_member(manager='base_objects').update_or_create(
-                chat_room_id=self.id,
-                user=user,
-                is_deleted=True,
-                defaults={'is_deleted': False}
-            )
-            return True
+            try:
+                self.chat_room_member(manager='non_removed_objects').update_or_create(
+                    chat_room_id=self.id,
+                    user=user,
+                    is_deleted=True,
+                    defaults={'is_deleted': False}
+                )
+                return True
+            except:
+                return False
         return False
 
     def add_new_member(self, user: UserModel) -> bool:
         """
-        add new member to chat room
-        update ChatRoomMember if user has left the chat room else create new
+        add new member to chat room if it is not removed
+        update ChatMember if user has left the chat room else create new
         """
         if not self.has_member(user):
-            self.chat_room_member(manager='base_objects').update_or_create(
-                chat_room_id=self.id,
-                user=user,
-                is_deleted=True,
-                defaults={'is_deleted': False}
-            )
-            return True
+            if self.is_ticket:
+                _manager = 'base_objects'
+            elif self.is_group:
+                _manager = 'non_removed_objects'
+            try:
+                self.chat_room_member(manager=_manager).update_or_create(
+                    chat_room_id=self.id,
+                    user=user,
+                    is_deleted=True,
+                    defaults={'is_deleted': False}
+                )
+                return True
+            except:
+                return False
         return False
 
     def remove_member(self, member: UserModel) -> bool:
@@ -301,22 +412,7 @@ class ChatRoom(RootModel):
             self.chat_room_member.filter(
                 chat_room_id=self.id,
                 user=member
-            ).update(is_deleted=True)
-            return True
-        return False
-
-    def leave_chat_room(self, member: UserModel) -> bool:
-        """
-        user leaves a chat room
-        """
-        if self.has_member(member):
-            self.chat_room_member.filter(
-                chat_room_id=self.id,
-                user=member
-            ).update(is_deleted=True)
-            # close the group if user was last member
-            if self.count_members == 0:
-                self.close()
+            ).update(is_deleted=True, action_permission=no_permission())
             return True
         return False
 
@@ -328,7 +424,7 @@ class ChatRoom(RootModel):
             self.chat_room_member.filter(
                 chat_room_id=self.id,
                 user=member
-            ).update(is_admin=True)
+            ).update(is_admin=True, action_permission=admin_permissions())
             return True
         return False
 
@@ -340,7 +436,7 @@ class ChatRoom(RootModel):
             self.chat_room_member.filter(
                 chat_room_id=self.id,
                 user=admin
-            ).update(is_admin=False)
+            ).update(is_admin=False, action_permission=member_permissions())
             return True
         return False
 
@@ -365,12 +461,22 @@ class ChatRoom(RootModel):
         db_table = 'chat_rooms'
 
 
-class ChatRoomMember(RootModel):
+class NonRemovedMemberManager(models.Manager):
+    """
+    custom manager for chat members who are not removed by admin
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().annotate(res=F('action_permission') % permission("join_group")).filter(res=0)
+
+
+class ChatMember(RootModel):
     """
     class model for chat room members
     """
     is_creator = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
+    action_permission = models.IntegerField(default=221)
     chat_room = models.ForeignKey(ChatRoom, related_name=_(
         'chat_room_member'), on_delete=models.CASCADE)
     user = models.ForeignKey(UserModel, related_name=_(
@@ -378,9 +484,17 @@ class ChatRoomMember(RootModel):
 
     objects = RootModelManager()
     base_objects = models.Manager()
+    non_removed_objects = NonRemovedMemberManager()
+
+    def delete(self, *args, **kwargs) -> tuple:
+        if self.is_creator:
+            self.chat_room.close_lock_delete()
+        self.is_deleted = True
+        self.save()
+        return tuple()
 
     @property
-    def role(self):
+    def role(self) -> str:
         """
         user role
         """
@@ -396,4 +510,3 @@ class ChatRoomMember(RootModel):
             models.UniqueConstraint(
                 fields=['chat_room', 'user'], name='unique_user_room')
         ]
-
