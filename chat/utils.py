4a37b09116f10a50a57e41c506b5886f9548619d
amirayat <amirayat20@gmail.com>
2023-04-15 21:51:29 +0330

    change blur_image function to in-memory files

--- chat/utils.py
+++ chat/utils.py
@@ -1,47 +1,55 @@
 import os
+import io
 import cv2
 from PIL import Image, ImageOps
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
-image_formats = [".jpg", ".jpeg", ".png", ".gif", ".tiff"]
-video_formats = [".mp4", ".mkv", ".avi", ".flv", ".f4v", ".swf", ".wmv", ".mov"]
-audio_formats = [".pcm", ".wav", ".aiff", ".mp3", ".aac", ".ogg",".wma",".flac",".alac"]
+image_formats = ["jpg", "jpeg", "png", "gif", "tiff"]
+video_formats = ["mp4", "mkv", "avi", "flv", "f4v", "swf", "wmv", "mov"]
+audio_formats = ["pcm", "wav", "aiff", "mp3",
+                 "aac", "ogg", "wma", "flac", "alac"]
 
 IMAGE_FORMATS = list()
 for format in image_formats:
     IMAGE_FORMATS.extend([format, format.upper()])
-IMAGE_FORMATS = tuple(IMAGE_FORMATS)
 
 VIDEO_FORMAT = list()
 for format in video_formats:
     VIDEO_FORMAT.extend([format, format.upper()])
-VIDEO_FORMAT = tuple(VIDEO_FORMAT)
+
+AUDIO_FORMAT = list()
+for format in audio_formats:
+    AUDIO_FORMAT.extend([format, format.upper()])
 
 
-def blur_image(img_path: str) -> str():
+def blur_image(file: InMemoryUploadedFile) -> InMemoryUploadedFile:
     """
     blur image and resize
     https://www.tutorialspoint.com/python_pillow/python_pillow_blur_an_image.htm
     https://stackoverflow.com/questions/273946/how-do-i-resize-an-image-using-pil-and-maintain-its-aspect-ratio
     https://stackoverflow.com/questions/46944107/how-to-crop-an-image-from-the-center-with-certain-dimensions
-    Gaussian Blur factor 10
     """
-    dir_name, base_name = os.path.split(img_path)
-    filename, file_format = os.path.splitext(img_path)
-    blured_img_path = dir_name + '/blured_' + base_name.replace(file_format, ".png")
-    img = Image.open(img_path)
-    # resize img
-    height, width = img.size
-    new_width  = 50
-    new_height = int(new_width * height / width) 
-    img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    image = Image.open(file)
+    # resize image
+    height, width = image.size
+    new_width = 50
+    new_height = int(new_width * height / width)
+    image = image.resize((new_width, new_height), Image.ANTIALIAS)
     wdif, hdif = 0, (new_height-new_width)//2
     border = wdif, hdif, wdif, hdif  # left, top, right, bottom
-    img = ImageOps.crop(img, border) 
-    # save img
-    img.save(blured_img_path) 
-
-    return blured_img_path
+    image = ImageOps.crop(image, border)
+    # convert to django image field
+    output = io.BytesIO()
+    image.save(output, format='PNG', quality=85)
+    output.seek(0)
+    return InMemoryUploadedFile(file=output,
+                                name=file.name,
+                                size=output.getbuffer().nbytes,
+                                content_type='image/png',
+                                charset=None,
+                                content_type_extra={},
+                                field_name='file_pic')
 
 
 def capture_video(vid_path: str) -> str():
