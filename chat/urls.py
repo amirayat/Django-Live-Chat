8dacfa6aeb42888b83131a3ca1762dcbc0ad3721
amirayat <amirayat20@gmail.com>
2023-04-16 18:23:12 +0330

    add top groups urls

--- chat/urls.py
+++ chat/urls.py
@@ -6,6 +6,7 @@ from chat.views import (AddMemberToGroupAPIView,
                         CloseGroupAPIView,
                         DemoteAdminAPIView,
                         GroupUpdateAPIView,
+                        TopPublicGroupViewSet,
                         GroupViewSet,
                         JoinPublicGroupAPIView,
                         LeaveGroupAPIView,
@@ -33,15 +34,15 @@ upload_router.register(r'', FileUploadView)
 
 
 urlpatterns = [
-    path('ticket/', include(ticket_router.urls)),
     path('ticket/<int:id>/close/', CloseLockTicketAPIView.as_view()),
     path('ticket/<int:id>/assign_staff/', AssignStaffToTicketAPIView.as_view()),
+    path('ticket/', include(ticket_router.urls)),
 
-    path('private_chat/', include(provate_chat_router.urls)),
     path('private_chat/<int:id>/block/', BlockUserAPIView.as_view()),
     path('private_chat/<int:id>/unblock/', UnBlockUserAPIView.as_view()),
+    path('private_chat/', include(provate_chat_router.urls)),
 
-    path('group/', include(group_router.urls)),
+    path('group/top/', TopPublicGroupViewSet.as_view()),
     path('group/<int:id>/update/', GroupUpdateAPIView.as_view()),
     path('group/<int:id>/close/', CloseGroupAPIView.as_view()),
     path('group/<int:id>/lock/', Un_LockGroupAPIView.as_view()),
@@ -51,6 +52,7 @@ urlpatterns = [
     path('group/<int:id>/promote/', PromoteMemberAPIView.as_view()),
     path('group/<int:id>/demote/', DemoteAdminAPIView.as_view()),
     path('group/<int:chat_room_id>/leave/', LeaveGroupAPIView.as_view()),
+    path('group/', include(group_router.urls)),
 
     path('group/<int:id>/permission/<int:user_id>/', MemberActionPermissionAPIView.as_view()),
 
